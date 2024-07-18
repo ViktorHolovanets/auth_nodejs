e242aa2ab7fcf9e5cc3a4164addb2391e2e51e5e
Viktor Holovanets <victorgolova@gmail.com>
2024-07-18 16:21:10 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,20 +1,15 @@
 import express from "express"
 import routes from "./route/index.js"
 import sequelize from "./config/database.js";
-import User from "./models/db/User.js"
-import { json } from "sequelize";
 const PORT = 5000;
 const app = express();
-sequelize.sync({ force: true }) // Установіть { force: true } для видалення існуючих таблиць та створення нових
+sequelize.sync() 
   .then(async () => {
-    console.log('Database & tables created!');
-
-    // Додавання деяких користувачів для тестування
-    /*await User.bulkCreate([
-      { firstName: 'John', lastName: 'Doe', email: 'john@example.com', password: '123456' },
-      { firstName: 'Jane', lastName: 'Doe', email: 'jane@example.com', password: 'abcdef' }
-    ]);*/
-  });
+    console.log('Database & tables created!');   
+  })
+  .catch(error => {
+    console.error('Error:', error);
+});;
 app.use(express.json())
 app.use("/", routes)
 app.listen(PORT, () => console.log(`Start Server on Port : ${PORT}`))
